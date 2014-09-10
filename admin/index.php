00d78df3bbaebd05ab4a8ae5d9195565806b5a11
panicroom <panayiotismenelaou@gmail.com>
2014-09-10 23:50:19 +0300

    Test

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@
         echo ("<A HREF=./index.php><img src='./images/settings.png' HEIGHT='100' WIDTH='100' border='0' alt='Link to this page'></A>");
         echo ("<A HREF=./new_dev.php><img src='./images/search.png' HEIGHT='100' WIDTH='100' border='0' alt='Link to this page'></A>");
         echo ("<div align='center'>");
-        $response = file_get_contents('http://localhost:8080/dev_list');
+        $response = file_get_contents('http://localhost:8080/dev_list/?test');
 //	$response = str_replace("(","",$response);
 //	$response = str_replace(")","",$response);
         for($i=0;$i < count($response);$i++){
