d3b37ce406c5f35af87f601df9d8872464b4b9d8
root <root@raspberrypi.(none)>
2014-09-10 20:34:30 +0000

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title>New Devices</title>
+ </head>
+ <body>
+<?php
+        echo ("<A HREF=./index.php><img src='./images/settings.png' HEIGHT='100' WIDTH='100' border='0' alt='Link to this page'></A>");
+        echo ("<A HREF=./new_dev.php><img src='./images/search.png' HEIGHT='100' WIDTH='100' border='0' alt='Link to this page'></A>");
+        echo ("<div align='center'>");
+        $response = file_get_contents('http://localhost:8080/test');
+//	$response = str_replace("(","",$response);
+//	$response = str_replace(")","",$response);
+        for($i=0;$i < count($response);$i++){
+            echo "<br>$response</br>";
+        }
+        echo "<br></br>";
+?>
+ </body>
+</html>
+
